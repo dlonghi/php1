dca5eceee49b4a36cf46d0bde8152b35797dad7f
dhl <daniel.longhi@trf2.jus.br>
2019-04-05 16:51:03 -0300

    message x

--- public/index.php
+++ public/index.php
@@ -7,8 +7,8 @@ function hint (int $A, float $B, string $C, bool $D)
 {
     var_dump($A, $B, $C, $D);
 }
-
-print hint(123, 1234, "abcd", $TRUE);
+echo "teste hint!<br>";
+echo hint(123, 1234, "abcd", $TRUE);
 
 ?>
 </body>
